62832b560f17125cd09f7bbc42335a89fae9f649
Vladimir <reactwebdew@gmail.com>
2022-11-12 17:59:33 +0300

    modals setup fix

--- src/page-components/Modal/NewWidget/NewWidget.tsx
+++ src/page-components/Modal/NewWidget/NewWidget.tsx
@@ -26,34 +26,32 @@ export const NewWidget = ({ className }: NewWidgetModalProps) => {
   }, [newWidget])
 
   return (
-    <div className={clsx(styles.newWidgetModal, className)}>
-      <div className={styles.modal}>
-        <div className={styles.title}>
-          <p>Новый виджет</p>
-          <ReactSVG
-            onClick={() => dispatch(setNewWidgetModal(false))}
-            src={close}
-          />
-        </div>
-        <div className={styles.wrapper}>
-          <div className={styles.address}>
-            <p>Адрес сайта:</p>
-            <input placeholder="tehnix.su" className={styles.input} />
-            <p>Сайт куда будет установлен виджет</p>
-          </div>
-          <div className={styles.toggles}>
-            <div className={styles.toggle}>
-              <p>Включить звонки</p>
-              <Toggle mode="on" />
-            </div>
-            <div className={styles.toggle}>
-              <p>Включить чат</p>
-              <Toggle mode="on" />
-            </div>
-          </div>
-        </div>
-        <ModalButtons />
+    <div className={clsx(styles.newWidget, className)}>
+      <div className={styles.title}>
+        <p>Новый виджет</p>
+        <ReactSVG
+          onClick={() => dispatch(setNewWidgetModal(false))}
+          src={close}
+        />
       </div>
+      <div className={styles.wrapper}>
+        <div className={styles.address}>
+          <p>Адрес сайта:</p>
+          <input placeholder="tehnix.su" className={styles.input} />
+          <p>Сайт куда будет установлен виджет</p>
+        </div>
+        <div className={styles.toggles}>
+          <div className={styles.toggle}>
+            <p>Включить звонки</p>
+            <Toggle mode="on" />
+          </div>
+          <div className={styles.toggle}>
+            <p>Включить чат</p>
+            <Toggle mode="on" />
+          </div>
+        </div>
+      </div>
+      <ModalButtons />
     </div>
   )
 }
--- src/page-components/Modal/OperatorData/OperatorData.tsx
+++ src/page-components/Modal/OperatorData/OperatorData.tsx
@@ -1,5 +1,32 @@
+import clsx from 'clsx'
+import { useDispatch } from 'react-redux'
+import { ReactSVG } from 'react-svg'
+import { setOperatorDataModal } from '../../../redux/modals/slice'
 import styles from './OperatorData.module.scss'
 
+import close from '../../../assets/icons/close-modal.svg'
+import { ModalButtons } from '../../ModalButtons/ModalButtons'
+
 export const OperatorData = () => {
-  return <div>OperatorData</div>
+  const dispatch = useDispatch()
+
+  return (
+    <div className={styles.newWidget}>
+      <div className={styles.title}>
+        <p>Новый виджет</p>
+        <ReactSVG
+          onClick={() => dispatch(setOperatorDataModal(false))}
+          src={close}
+        />
+      </div>
+      <div className={styles.wrapper}>
+        <div className={styles.address}>
+          <p>Адрес сайта:</p>
+          <input placeholder="tehnix.su" className={styles.input} />
+          <p>Сайт куда будет установлен виджет</p>
+        </div>
+      </div>
+      <ModalButtons />
+    </div>
+  )
 }
